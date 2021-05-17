4066ef751ed4657a944a099dd9f1cf0fd559de1d
sandrabachinger-raithofer <sandra.bachinger-raithofer@stud.fhcampus-wien.ac.at>
2021-05-17 14:33:15 +0200

    3 new testcases

--- src/main/java/at/ac/fhcampuswien/Password.java
+++ src/main/java/at/ac/fhcampuswien/Password.java
@@ -27,15 +27,15 @@ public class Password {
     }
 
     boolean passwordLength(){
-        if (this.password.length()> 8 && this.password.length()<25){
+        if (this.getPassword().length()> 8 && this.getPassword().length()<25){
             return true;
         }
         else return false;
     }
 // help from https://stackoverflow.com/questions/40336374/how-do-i-check-if-a-java-string-contains-at-least-one-capital-letter-lowercase
     boolean checkUpperAndLowerCase(){
-        boolean lowerCase = this.password.matches(".*[A-Z].*");
-        boolean upperCase = this.password.matches(".*[a-b].*");
+        boolean lowerCase = this.getPassword().matches(".*[A-Z].*");
+        boolean upperCase = this.getPassword().matches(".*[a-b].*");
         if(lowerCase && upperCase) {
             return true;
         }
@@ -43,7 +43,7 @@ public class Password {
     }
 
     boolean checkContainNumber(){
-       return this.password.matches(".*[1-9].*");
+       return this.getPassword().matches(".*[1-9].*");
     }
 
     boolean checkNumbersCont(){
@@ -54,10 +54,10 @@ public class Password {
         //help from https://stackoverflow.com/questions/4047808/what-is-the-best-way-to-tell-if-a-character-is-a-letter-or-number-in-java-withou
         else
             // look for a number in the string, then check if the two foolowing chars are also numbers, if so, add all numbers and devide by 3, if they equal the number
-        //after the first number the numbers are continous. then retrun false
-            for (int i = 0; i < this.password.length()-2; i++){
-            if (Character.isDigit(this.password.charAt(i))&& Character.isDigit(this.password.charAt(i+1)) && Character.isDigit(this.password.charAt(i+2))){
-                if (((this.password.charAt(i))+(this.password.charAt(i+1))+(this.password.charAt(i+2)))/3 == (this.password.charAt(i+1))) {
+        //after the first number the numbers are continous. then return false
+            for (int i = 0; i < this.getPassword().length()-2; i++){
+            if (Character.isDigit(this.getPassword().charAt(i))&& Character.isDigit(this.getPassword().charAt(i+1)) && Character.isDigit(this.getPassword().charAt(i+2))){
+                if (((this.getPassword().charAt(i))+(this.getPassword().charAt(i+1))+(this.getPassword().charAt(i+2)))/3 == (this.getPassword().charAt(i+1))) {
                     return false;
                 }
             }
@@ -72,11 +72,11 @@ public class Password {
 
         //help from https://stackoverflow.com/questions/4047808/what-is-the-best-way-to-tell-if-a-character-is-a-letter-or-number-in-java-withou
         else
-            // look for a number in the string, then check if the two foolowing chars are also numbers, if so, add all numbers and devide by 3, if they equal the number
-            //after the first number the numbers are continous. then retrun false
-            for (int i = 0; i < this.password.length()-3; i++){
-                if (Character.isDigit(this.password.charAt(i))&& Character.isDigit(this.password.charAt(i+1)) && Character.isDigit(this.password.charAt(i+2))&& Character.isDigit(this.password.charAt(i+3))){
-                    if (((this.password.charAt(i))+(this.password.charAt(i+1))+(this.password.charAt(i+2))+(this.password.charAt(i+3)))/4 == (this.password.charAt(i))) {
+            // look for a number in the string, then check if the three foolowing chars are also numbers, if so, add all numbers and devide by 4, if they equal the first number
+            //the numbers are the same. then return false
+            for (int i = 0; i < this.getPassword().length()-3; i++){
+                if (Character.isDigit(this.getPassword().charAt(i))&& Character.isDigit(this.getPassword().charAt(i+1)) && Character.isDigit(this.getPassword().charAt(i+2))&& Character.isDigit(this.getPassword().charAt(i+3))){
+                    if (((this.getPassword().charAt(i))+(this.getPassword().charAt(i+1))+(this.getPassword().charAt(i+2))+(this.getPassword().charAt(i+3)))/4 == (this.getPassword().charAt(i))) {
                         return false;
                     }
                 }
@@ -85,8 +85,8 @@ public class Password {
     }
 
     boolean containSpecialCharacter(){
-        boolean containsSpecialCharacter = this.password.matches(".*[!@#$%()?/].*");
-        boolean containsNoOtherSpecialCharacter = this.password.matches("[a-zA-Z0-9!@#$%()?/]*");
+        boolean containsSpecialCharacter = this.getPassword().matches(".*[!@#$%()?/].*");
+        boolean containsNoOtherSpecialCharacter = this.getPassword().matches("[a-zA-Z0-9!@#$%()?/]*");
 
         if (containsSpecialCharacter&&containsNoOtherSpecialCharacter){
             return true;
