6bac50bfe632467da7960c02348dc50b90b3d9fe
sandrabachinger-raithofer <sandra.bachinger-raithofer@stud.fhcampus-wien.ac.at>
2021-05-17 08:13:08 +0200

    Initial commit

---
+++ src/main/java/at/ac/fhcampuswien/PasswordValidator.java
@@ -0,0 +1,7 @@
+package at.ac.fhcampuswien;
+
+public class PasswordValidator {
+    public static void main(String [] args){
+
+    }
+}
---
+++ src/test/java/at/ac/fhcampuswien/PasswordValidatorTest.java
@@ -0,0 +1,13 @@
+package at.ac.fhcampuswien;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PasswordValidatorTest {
+
+  @Test
+    public void myTest(){
+    assertTrue(true);
+      
+  }
+}
